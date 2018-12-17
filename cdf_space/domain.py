bf37ec539deda6b6f9994eb21bbcbbf233a7b6f7
tarkanii <nastyatrko@gmail.com>
2018-12-17 15:25:39 +0200

    Change default bandwidth parameter (h) calculation
    [^] cdf_space/
    ----[^] domain.py

--- cdf_space/domain.py
+++ cdf_space/domain.py
@@ -10,36 +10,40 @@ def euclidean_distance(p1, p2, axis=0):
 
 class Domain:
 
-    @property
-    def undefined(self):
-        raise KeyError("Domain: trying to get undefined property")
-
-    get_distance = undefined
-
     def __init__(self, data, kernel, dist_function=euclidean_distance):
-        if not isinstance(data, np.ndarray):
-            data = np.array(data)
-            if not isinstance(data[0], np.ndarray):
-                raise ValueError("Argument `data` should be convertible to 2-dimensional numpy array")
-
-        if data.size == 0:
-            raise ValueError("Argument `data` can't be empty")
-
-        if not isinstance(kernel, BaseKernel):
-            raise ValueError("kernel should be an instance of cdf_space.kernels.BaseKernel class")
-
-        if not callable(dist_function) or not isinstance(dist_function(data[0], data[0]), numbers.Number):
-            raise ValueError(
-                "dist_function should be callable object which returns numeric value".format(type(dist_function))
-            )
-
         self.__data = data
         self.__get_distance = dist_function
-
         self.__kernel = kernel
 
+        self.__clean()
+
+    def __clean(self):
+        if not isinstance(self.__data, np.ndarray):
+            self.__data = np.array(self.__data)
+            if not isinstance(self.__data[0], np.ndarray):
+                raise ValueError("Argument `data` should be convertible to 2-dimensional numpy array")
+
+        if self.__data.size == 0:
+            raise ValueError("Argument `data` can't be empty")
+
+        if not isinstance(self.__kernel, BaseKernel):
+            raise ValueError("kernel should be an instance of cdf_space.kernels.BaseKernel class")
+
+        if not callable(self.__get_distance) or \
+                not isinstance(self.__get_distance(self.__data[0], self.__data[0]), numbers.Number):
+            raise ValueError(
+                "dist_function should be callable object which returns numeric value".format(type(self.__get_distance))
+            )
+
     def __calculate_default_h(self):
-        return np.max(self.__data) - np.min(self.__data)
+        max_dist = -float('inf')
+
+        for x in range(self.__data.shape[0]):
+            for x_i in range(x + 1, self.__data.shape[0]):
+                _dist = self.__get_distance(self.__data[x], self.__data[x_i])
+                max_dist = _dist if _dist > max_dist else max_dist
+
+        return max_dist
 
     def __generate_pdf_matrix_by_row(self, h, dtype, normalized_constant):
         data_size = self.__data.shape[0]
@@ -47,11 +51,11 @@ class Domain:
 
         pdf_matrix = np.zeros((data_size,), dtype=dtype)
 
-        for i in data_size:
+        for i in range(data_size):
             _current_point_repeated = np.empty(self.__data.shape, self.__data.dtype)
             _current_point_repeated[:,:] = self.__data[i]
 
-            _dist = self.get_distance(_current_point_repeated, self.__data, axis=1) / h
+            _dist = self.__get_distance(_current_point_repeated, self.__data, axis=1) / h
             pdf_matrix[i] = self.__kernel.estimate_density_row(_dist)
             pdf_matrix[i] *= normalized_constant / (data_size * h ** dim)
 
@@ -62,6 +66,7 @@ class Domain:
         dim = self.__data.data.shape[1]
 
         h = h or self.__calculate_default_h()
+        print(h)
         normalized_constant = self.__kernel.calculate_normalized_constant(dim)
 
         if by_row:
@@ -69,9 +74,9 @@ class Domain:
 
         pdf_matrix = np.zeros((data_size,), dtype=dtype)
 
-        for i in data_size:
-            for j in data_size:
-                pdf_matrix[i] += self.__kernel.estimate_density(self.get_distance(data_size[i], data_size[j]) / h)
+        for i in range(data_size):
+            for j in range(data_size):
+                pdf_matrix[i] += self.__kernel.estimate_density(self.__get_distance(self.__data[i], self.__data[j]) / h)
             pdf_matrix[i] *= normalized_constant / (data_size * h ** dim)
 
         return pdf_matrix
